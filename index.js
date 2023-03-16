f6e7f65298f3d097d3a59448cc8808ee01919667
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-16 22:36:43 +0530

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("AWS Testing");
+})
+
+app.listen(3030, ()=>{
+    console.log("server is running at port 3030");
+})
