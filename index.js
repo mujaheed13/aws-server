13cdde0028413ba0fc17c4934b6c163014fdd151
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-17 15:58:24 +0530

    aws2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("AWS Testing");
+    res.send("AWS Testing with server 2");
 })
 
 app.listen(3030, ()=>{
