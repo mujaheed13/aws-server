fdabd0a578a12b97a8c07ac218704b28cf8efe90
Mohammad Mujaheed <112754547+mujaheed13@users.noreply.github.com>
2023-03-18 11:13:02 +0530

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("AWS Testing with server 2");
+    res.send("AWS Testing with server 1");
 })
 
 app.listen(3030, ()=>{
